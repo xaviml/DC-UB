1aade8ecaa5b6439be761dc5df3465d270c86929
Pablo <Pablo@Laptop>
2014-02-24 11:58:56 +0100

    Game ending now controlled.

--- SD_P1_Server/src/model/game/GameController.java
+++ SD_P1_Server/src/model/game/GameController.java
@@ -22,25 +22,35 @@ public class GameController implements GameControllerInterface{
     }
 
     @Override
-    public void endGame() {
+    public Turn endGame(Turn t) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 
     @Override
     public Turn throwing(DominoPiece piece, Pieces.Side side) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Turn t =  game.throwing(piece, side);
+        if (t.gameEndFlag){
+            t = endGame(t);
+        }
+        return t;
     }
 
 
     @Override
     public Turn steal() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /* Check if game has ended in a Draw*/
+        Turn t = game.steal();
+        if ((t.computerCantPlayFlag && t.playerCantPlayFlag) || t.gameEndFlag){
+            return endGame(t);
+        }
+        
+        return game.steal();
     }
 
     @Override
     public Turn initGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return game.initGame();
     }
     
 }
