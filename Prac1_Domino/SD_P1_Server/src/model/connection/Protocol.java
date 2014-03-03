c96e690c342dc4df4a06519104d55809a343a7f6
kirtashdark <pmartima22@alumnes.ub.edu>
2014-03-03 23:36:02 +0100

    AbstractProtocol methods implemented 0%.

--- Prac1_Domino/SD_P1_Server/src/model/connection/Protocol.java
+++ Prac1_Domino/SD_P1_Server/src/model/connection/Protocol.java
@@ -6,6 +6,7 @@
 package model.connection;
 
 import java.io.IOException;
+import model.game.Turn;
 
 /**
  * Protocol. This class will be responsible to get all messages from the socket
@@ -19,6 +20,46 @@ public class Protocol extends AbstractProtocol{
     public Protocol (Connection c) throws IOException{
         super(c.getSocket());
         this.connection = c;
+    }
+
+    @Override
+    public void helloFrameRequest(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gamePlayRequest(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gameStealRequest(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void helloFrameResponse(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gamePlayResponse(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gameStealResponse(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gameFinishedResponse(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void errorResponse(Turn t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
