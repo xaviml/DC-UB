85574cac770fec22a54ab030c3da10590ca6b36f
kirtashdark <pmartima22@alumnes.ub.edu>
2014-03-11 13:50:43 +0100

    Time to Timeout reduced in the readFunction.

---
+++ Prac1_Domino/LibraryDP/src/ub/swd/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ub.swd;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Constants {
+    public static final int SOCKET_TO_WHILE_READING = 500;
+            
+}
