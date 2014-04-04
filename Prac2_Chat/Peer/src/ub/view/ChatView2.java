09cf79e634d52adf306d0698cca0f5eba2830648
xaviml <xaviml.93@gmail.com>
2014-04-04 02:14:37 +0200

    GUI prototype

--- Prac2_Chat/Peer/src/ub/view/ChatView2.java
+++ Prac2_Chat/Peer/src/ub/view/ChatView2.java
@@ -8,21 +8,40 @@ package ub.view;
  
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import ub.controller.ChatController;
  
-public class MainView extends JFrame implements ActionListener {
+public class ChatView2 extends JFrame implements ActionListener {
  
     private String screenName;
 
     // GUI stuff
     private JTextArea enteredText = new JTextArea(10, 32);
     private JTextField typedText = new JTextField(32);
+    
+    
+    private JTabbedPane jTabs;
+    private JList users;
  
-    public MainView(String screenName, String hostName) {
+    public ChatView2(String screenName, String hostName) {
+        
+        
+        users = new JList(new DefaultListModel());
+        addUser("Xavi");
+        addUser("ASDFAISDFIASDFH");
+        users.setSize(new Dimension(100, 100));
+        
+        DefaultListCellRenderer renderer =  (DefaultListCellRenderer)users.getCellRenderer();  
+        renderer.setHorizontalAlignment(JLabel.CENTER); 
+        
 
     // close output stream - this will cause listen() to stop and exit
         this.screenName = screenName;
@@ -42,6 +61,7 @@ public class MainView extends JFrame implements ActionListener {
         Container content = getContentPane();
         content.add(new JScrollPane(enteredText), BorderLayout.CENTER);
         content.add(typedText, BorderLayout.SOUTH);
+        content.add(users, BorderLayout.WEST);
 
 
         // display the window, with focus on typing box
@@ -52,8 +72,19 @@ public class MainView extends JFrame implements ActionListener {
         setVisible(true);
 
     }   
+    
+    private void createNewTab(String title, JComponent content) {
+        
+    }
+    
+    private void addUser(String user) {
+        DefaultListModel model = (DefaultListModel) users.getModel();
+        model.addElement(user);
+        
+    }
  
     // process TextField after user hits Enter
+    @Override
     public void actionPerformed(ActionEvent e) {
         String s = "[" + screenName + "]: " + typedText.getText();
         //System.out.println(s);
@@ -65,12 +96,13 @@ public class MainView extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        MainView client;
+        ChatView2 client;
         /*if (args.length!= 2)
             System.out.println("java GuiChat <nickname> <host>");
         else
-            client = new MainView(args[0], args[1]);*/
+            client = new ChatView2(args[0], args[1]);*/
+        client = new ChatView2("xaviml", null);
         
-        ChatController c = new ChatController("localhost", "xaviml", null, null);
+        //ChatController c = new ChatController("localhost", "xaviml", null, null);
     }
 }
