4810525e1933f33f48e700ffea3393dc8a9d40e9
kirtashdark <pmartima22@alumnes.ub.edu>
2014-04-08 15:10:02 +0200

    Modified code

--- Prac2_Chat/Peer/src/ub/controller/ChatController.java
+++ Prac2_Chat/Peer/src/ub/controller/ChatController.java
@@ -11,6 +11,7 @@ import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import ub.common.GroupReference;
 import ub.common.IServer;
 import ub.common.InvalidUserNameException;
 
@@ -27,20 +28,19 @@ public class ChatController {
     public ChatController(ChatRoomListener listener) {
         this.chatModel = new ChatModel(listener);
     }
-    
     public void register(String IP, int port, String username) throws RemoteException, NotBoundException, MalformedURLException, InvalidUserNameException {
         this.myUserName = username;
         chatModel.register(IP, port, username);
     }
-    
     public void disconnect(){
         chatModel.disconnect();
     }
-    
     public void writeMessage(String username, String message){
         chatModel.writeMessage(username, message);
     }
-    
+    public void writeMessage(GroupReference gref, String message){
+        chatModel.writeMessage(gref, message);
+    }
     public String getUsername(){
         return myUserName;
     }
