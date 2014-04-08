003326f4a9cdca983e593ca08a98b94b3b1dc7c2
kirtashdark <pmartima22@alumnes.ub.edu>
2014-04-08 16:41:12 +0200

    Chat model changed slightly.

--- Prac2_Chat/Peer/src/ub/model/ChatModelServices.java
+++ Prac2_Chat/Peer/src/ub/model/ChatModelServices.java
@@ -3,6 +3,7 @@ package ub.model;
 import ub.common.IPeer;
 
 public interface ChatModelServices{
+    public String getMyUserName();
     public IPeer getIPeerByName(String username);
     public void notifyDisconnectedClient(String username);
 }
